81f2bee849e8013144f81b9146ed14396ccc54f4
UniJohannes <jod7472@thi.de>
2026-05-02 16:51:09 +0200

    routes fix

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -8,9 +8,9 @@ import { ProfilPage } from "@/features/profil/components/ProfilPage";
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<HomeView />} />
-      <Route path="/angebote" element={<AngebotePage />} />
       <Route path="/aufnahme" element={<AufnahmePage />} />
+      <Route path="/angebote" element={<AngebotePage />} />
+      <Route path="/" element={<HomeView />} />
       <Route path="/unternehmen" element={<UnternehmenPage />} />
       <Route path="/profil" element={<ProfilPage />} />
     </Routes>
