57001fee156966aa91679840fc03e60e6c636fce
Marvin <mak2868@thi.de>
2026-06-03 22:44:47 +0200

    PE Anbindung

--- processengine/src/main/java/org/cibseven/getstarted/loanapproval/WebappExampleProcessApplication.java
+++ processengine/src/main/java/org/cibseven/getstarted/loanapproval/WebappExampleProcessApplication.java
@@ -16,28 +16,21 @@
  */
 package org.cibseven.getstarted.loanapproval;
 
-import org.cibseven.bpm.engine.RuntimeService;
 import org.cibseven.bpm.spring.boot.starter.annotation.EnableProcessApplication;
-import org.cibseven.bpm.spring.boot.starter.event.PostDeployEvent;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.event.EventListener;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableProcessApplication
-public class WebappExampleProcessApplication {
+@ComponentScan(basePackages = {
+        "org.cibseven.getstarted.loanapproval",
+        "de.winfprojekt.craftvoice"
+})
 
-  @Autowired
-  private RuntimeService runtimeService;
+public class WebappExampleProcessApplication {
 
   public static void main(String... args) {
     SpringApplication.run(WebappExampleProcessApplication.class, args);
   }
-
-  @EventListener
-  public void processPostDeploy(PostDeployEvent event) {
-    runtimeService.startProcessInstanceByKey("loanApproval");
-  }
-
 }
