f0a5c0b959b5c477f8e5264910fe7d6d9a30a3b4
linkspreed <developer@linkspreed.com>
2026-06-12 10:47:00 +0200

    customer tests

--- backend/services/user-service/src/main/java/de/winfprojekt/craftvoice/userservice/UserResource.java
+++ backend/services/user-service/src/main/java/de/winfprojekt/craftvoice/userservice/UserResource.java
@@ -9,6 +9,7 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.jwt.JsonWebToken;
+import java.util.List;
 import java.util.Map;
 
 @Path("/api/users")
