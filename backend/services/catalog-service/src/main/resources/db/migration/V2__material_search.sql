da2fdc17fcfdaa9a03e895281bb80f2539f171e4
Michi <mip0006@thi.de>
2026-06-21 16:13:56 +0200

    DB-migration aktualisiert

--- backend/services/catalog-service/src/main/resources/db/migration/V2__material_search.sql
+++ backend/services/catalog-service/src/main/resources/db/migration/V2__material_search.sql
@@ -4,12 +4,5 @@ CREATE INDEX IF NOT EXISTS idx_material_name_trgm
 CREATE INDEX IF NOT EXISTS idx_material_description_trgm
     ON material USING gin (description gin_trgm_ops);
 
-CREATE INDEX IF NOT EXISTS idx_material_search
-    ON material
-    USING gin (
-    (
-    setweight(to_tsvector('german', unaccent(coalesce(name, ''))), 'A') ||
-    setweight(to_tsvector('german', unaccent(coalesce(description, ''))), 'B') ||
-    setweight(to_tsvector('german', unaccent(coalesce(category, ''))), 'C')
-    )
-    );
+CREATE INDEX IF NOT EXISTS idx_material_category_trgm
+    ON material USING gin (category gin_trgm_ops);
