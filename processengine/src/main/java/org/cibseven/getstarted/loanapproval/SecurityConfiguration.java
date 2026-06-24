27477044638631b27e28779068bd9fe20c22e0d1
deezify <lennartmoog04+github@gmail.com>
2026-06-25 00:58:48 +0200

    Anpassung, dass CIB-seven eine UI hat, die unter https://pe-craftvoice.winfprojekt.de/webapp/ erreichbar ist.

--- processengine/src/main/java/org/cibseven/getstarted/loanapproval/SecurityConfiguration.java
+++ processengine/src/main/java/org/cibseven/getstarted/loanapproval/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package org.cibseven.getstarted.loanapproval;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,6 +20,7 @@ public class SecurityConfiguration {
         this.filter = filter;
     }
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
