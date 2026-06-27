986a8cdbe4cd442f2d6d6107d2094da88693f52d
Marvin Kunz <mak2868@thi.de>
2026-06-27 13:15:24 +0200

    Update OfferService.java

--- backend/services/offer-service/src/main/java/de/winfprojekt/craftvoice/offerservice/offer/OfferService.java
+++ backend/services/offer-service/src/main/java/de/winfprojekt/craftvoice/offerservice/offer/OfferService.java
@@ -683,6 +683,13 @@ public class OfferService {
 
         offer.persist();
 
+        // Process Engine benachrichtigen 
+        if (Offer.STATUS_ANGENOMMEN.equals(targetStatus)) {
+            processEngineClient.sendAngebotAngenommen(offer.businessKey); 
+        } else { 
+            processEngineClient.sendAngebotAbgelehnt(offer.businessKey); 
+        }
+
     }
 
     /**
