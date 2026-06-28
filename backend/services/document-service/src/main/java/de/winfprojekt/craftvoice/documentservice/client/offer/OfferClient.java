1cd5af12ba0cbbbdda198d65ea301d88b2c143b2
deezify <lennartmoog04+github@gmail.com>
2026-06-28 18:46:40 +0200

    Documnet Service hat bisher nicht berücksichtigt, dass der Status versandbereit gesetzt werden muss. Schnittstelle wird jetzt korrekt aufgerufen.  Zudem zeigt die Angebots-PDF auch wieder die geforderten Positionen an.

---
+++ backend/services/document-service/src/main/java/de/winfprojekt/craftvoice/documentservice/client/offer/OfferClient.java
@@ -0,0 +1,22 @@
+package de.winfprojekt.craftvoice.documentservice.client.offer;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@Path("/")
+@RegisterRestClient(configKey = "offer-service")
+public interface OfferClient {
+
+    @POST
+    @Path("/angebote/{businessKey}/versandbereit")
+    @Consumes(MediaType.WILDCARD)
+    void setStatusVersandbereit(
+            @PathParam("businessKey") String businessKey,
+            @HeaderParam("Authorization") String authorizationHeader
+    );
+}
